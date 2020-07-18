a2ef6264ea6e126acbcf6232b18e01807bfa9f0e
Brian Crounse <bcrounse@wayfair.com>
2020-07-17 23:50:24 -0400

    Added and completed some data loading functions

--- src/cog.py
+++ src/cog.py
@@ -1,8 +1,10 @@
 
+import os
 import sys
 import yaml
-import pandas
+import pandas as pd
 import folium
+
 
 def get_parameters(file_config, path='../cfg'):
     """
@@ -12,17 +14,56 @@ def get_parameters(file_config, path='../cfg'):
     """
 
     # Load YAML file...
+    with open(os.path.join(path, file_config)) as file:
+        params = yaml.load(file, Loader=yaml.SafeLoader)
 
     return params
 
-def load_demand_data(file_data, path='../data'):
+
+def load_demand_data(file_data, path='../data', geo_fld='zip5', dmd_fld='dmd', dmd_fld_norm='dmd_pct'):
     """
+
+    Loads demand data, which consists of a geo ID (typically postal code) and a demand weight
 
     :param file_data: Name of data file
     :param path: Path for data file
+    :param geo_fld: Name of field with geographical location in returned df
+    :param dmd_fld: Name of field with demand in returned df
+    :param dmd_fld_norm: Name of field with normalized demand info
     :return: df with demand data
     """
 
+    df = pd.read_csv(os.path.join(path, file_data))
+
+    # Replace field names based on any number of possibilities
+    list_of_zip_fields = ['Zip Code ZCTA']
+    for f in list_of_zip_fields:
+        df.rename(inplace=True, columns={f: geo_fld})
+
+    list_of_dmd_fields = ['2010 Census Population']
+    for f in list_of_dmd_fields:
+        df.rename(inplace=True, columns={f: dmd_fld})
+
+    # Normalize demand
+    df[dmd_fld_norm] = df[dmd_fld] / df[dmd_fld].sum()
+
+    return df
+
+
+def load_supply_data(file_data, path='../data', geo_fld='zip5', dmd_fld='dmd', dmd_fld_norm='dmd_pct'):
+    """
+
+    :param file_data:
+    :param path:
+    :param geo_fld:
+    :param dmd_fld:
+    :param dmd_fld_norm:
+    :return:
+    """
+
+    df = pd.read_csv(os.path.join(path, file_data))
+
+    return df
 
 if __name__ == "__main__":
 
@@ -36,5 +77,14 @@ if __name__ == "__main__":
 
     dict_params = get_parameters(cfg)
 
-    data_dmd = load_demand_data(dict_params['file_demand'])
+    # This is a little ugly but lets us respect default values in the function
+    if 'path_data' in dict_params.keys():
+        data_dmd = load_demand_data(dict_params['file_demand'], path=dict_params['path_data'])
+    else:
+        data_dmd = load_demand_data(dict_params['file_demand'])
 
+    if dict_params['flag_supply']:
+        if 'path_data' in dict_params.keys():
+            data_supply = load_supply_data(dict_params['file_demand'], path=dict_params['path_data'])
+        else:
+            data_supply = load_supply_data(dict_params['file_demand'])
